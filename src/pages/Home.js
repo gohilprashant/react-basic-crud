7cc0c0a2f257e518814245602b1e23192c2eebd6
Prashant Gohil <prashantgohil248@gmail.com>
2023-02-06 23:37:14 +0530

    Remove extra code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,6 @@ import { toast } from 'react-toastify';
 import PostCard from '../components/post/PostCard';
 import { useGetPosts } from '../services/useGetPosts';
 
-const post = {
-  id: '5903abf4-c73b-4758-a3ef-f255b8aa7f0f',
-  title: 'Asperiores quisquam accusantium ab porro earum optio magni veritatis animi.',
-  body: 'Ut laborum expedita. Inventore non enim sed animi eius quo mollitia voluptatem consequuntur. Asperiores fugiat non.\nAliquam quia alias placeat. Esse sequi iure odio porro ab at. Omnis modi omnis alias vitae optio dolore asperiores modi. Ipsa qui sapiente vel eius. Sequi deserunt impedit.\nMagni corrupti impedit molestiae facilis. Sapiente corporis occaecati excepturi nam odio. Ullam unde est.',
-  category: 'Outdoors',
-};
-
 const Home = () => {
   const { data, isLoading, isError } = useGetPosts();
 
